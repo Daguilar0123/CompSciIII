8d6e4c5856d997d28ca8a7191e3bc7baae5000e8
Daguilar0123 <aguilard107@gmail.com>
2025-04-09 00:23:56 -0400

    Add input prompt and error initialization to DelimeterChecker

--- DelimeterChecker.py
+++ DelimeterChecker.py
@@ -1,3 +1,7 @@
 from SimpleStack import *
 
 stack = Stack(100)          # Create a stack to hold delimeter tokens
+
+expr = input("Expression to check: ")
+
+errors = 0          # Assume no errors in expression
