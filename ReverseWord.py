d28f6f37e6b02edc486b19081bd3191427f17c42
Daguilar0123 <aguilard107@gmail.com>
2025-04-09 00:18:02 -0400

    deleted comment

--- ReverseWord.py
+++ ReverseWord.py
@@ -5,7 +5,7 @@ stack = Stack(100)              # Create a stack to hold letters
 word = input("Word to reverse: ")
 
 for letter in word:             # Loop over letters in word
-    if not stack.isFull():      # Push letters on stack if not full
+    if not stack.isFull():
         stack.push(letter)
 
 reverse = ''                    # Build the reversed version
