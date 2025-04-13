1fc55f1e707340a2b08bd4c130757cad6693c744
Daguilar0123 <aguilard107@gmail.com>
2025-04-13 01:59:00 -0400

    Add LinkedList client script for basic functionality testing

---
+++ LinkedListClient.py
@@ -0,0 +1,13 @@
+from LinkedList import *
+
+def main():
+    ll = LinkedList()
+
+    ll.insert("First Element")
+    ll.insert("Second Element")
+    ll.insert("Third Element")
+
+    print(ll)
+
+if __name__=="__main__":
+    main()
