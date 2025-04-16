47d99106287d816baa0915da6f204eb473506214
Daguilar0123 <aguilard107@gmail.com>
2025-04-15 23:55:10 -0400

    Refactor Array class: streamline methods and improve error handling

--- Array.py
+++ Array.py
@@ -1,48 +1,42 @@
+# Implement an Array data structure as a simplified type of list.
 class Array(object):
-    def __init__(self, capacity):
-        self.__capacity = capacity
-        self.__a = [None] * capacity
-        self.__nItems = 0
-
-    def __len__(self):
-        return self.__nItems
-
-    def traverse(self, function=print):
-        for j in range(self.__nItems):
+    def __init__(self, initialSize): # Constructor
+        self.__a = [None] * initialSize # The array stored as a list
+        self.__nItems = 0 # No items in array initially
+        
+    def __len__(self): # Special def for len() func
+        return self.__nItems # Return number of items
+        
+    def get(self, n): # Return the value at index n
+        if 0 <= n and n < self.__nItems: # Check if n is in bounds, and
+            return self.__a[n] # only return item if in bounds
+            
+    def set(self, n, value): # Set the value at index n
+        if 0 <= n and n < self.__nItems: # Check if n is in bounds, and
+            self.__a[n] = value # only set item if in bounds
+                
+    def insert(self, item): # Insert item at end
+        self.__a[self.__nItems] = item # Item goes at current end
+        self.__nItems += 1 # Increment number of items
+            
+    def find(self, item): # Find index for item
+        for j in range(self.__nItems): # Among current items
+            if self.__a[j] == item: # If found,
+                return j # then return index to item
+        return -1 # Not found -> return -1
+        
+    def search(self, item): # Search for item
+        return self.get(self.find(item)) # and return item if found
+        
+    def delete(self, item): # Delete first occurrence
+        for j in range(self.__nItems): # of an item
+            if self.__a[j] == item: # Check if item at index j equals item
+                self.__nItems -= 1 # If found, decrement number of items by 1
+                for k in range(j, self.__nItems): # Move items from
+                    self.__a[k] = self.__a[k+1] # right over 1
+                return True # Return success flag
+        return False # Made it here, so couldn’t find the item
+                
+    def traverse(self, function=print): # Traverse all items
+        for j in range(self.__nItems): # and apply a function
             function(self.__a[j])
-
-    def get(self, n):                   # Return the value at index n
-        if 0<=n and n<self.__nItems:    # Check if n is in bounds
-            return self.__a[n]          # Return if in bounds
-        raise IndexError("Index out of bounds")
-        
-    def set(self, n, value):                # Set the value at index n
-        if 0<=n and n<self.__nItems:        # Check if n is in bounds
-            self.__a[n] = value             # only set value if in bounds
-        else:
-            raise IndexError("Index out of bounds")
-    def insert(self, item):                 # Insert item at end
-        if self.isFull():
-            raise OverflowError("Array is full")
-        else:
-            self.__a[self.__nItems] = item
-            self.__nItems += 1
-    def find(self, item):                   # Find index for item
-        for j in range(self.__nItems):
-            if self.__a[j] == item:
-                return j
-            return -1
-    def search(self, item):                 # Search for item
-        return self.get(self.find(item))    # and return item if found
-    def delete(self, item):
-        for j in range(self.__nItems):
-            if self.__a[j] == item:
-                self.__nItems -= 1
-                for k in range(j, self.__nItems):
-                    self.__a[k] = self.__a[k+1]
-                return True
-            return False
-        
-    def isFull(self):
-        return self.__nItems == self.__capacity
-     
