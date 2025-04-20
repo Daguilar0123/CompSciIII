388008664b48e78bd1d74c986a0ec1de14ea6332
Daguilar0123 <aguilard107@gmail.com>
2025-04-19 21:10:31 -0400

    Update file permissions for multiple Python scripts, JSON files, and other project files

--- BST.py
+++ BST.py
@@ -25,49 +25,49 @@ class BST:
                 _print_tree(node.right,level+1)
                 print(" "*level+ f"->{node.key}")
                 _print_tree(node.left,level+1)
-            _print_tree(self.root)
+        _print_tree(self.root)
 
-            # helper function to facilitate tree traversals
-            # The outside function is a public interface for the helper function
-            # outside (public) function
-            def pre_order(self): # root left right for printing a copy
-                # helper (private) function
-                def _preorder(node):
-                    if node:
-                        print(node.key,end="")
-                        _preorder(node.left)
-                        _preorder(node.right)
-                _preorder(self.root)
-                print()
+        # helper function to facilitate tree traversals
+        # The outside function is a public interface for the helper function
+        # outside (public) function
+        def pre_order(self): # root left right for printing a copy
+            # helper (private) function
+            def _preorder(node):
+                if node:
+                    print(node.key,end="")
+                    _preorder(node.left)
+                    _preorder(node.right)
+            _preorder(self.root)
+            print()
 
-            def in_order(self): # left node right good for sorted order
-                def _inorder(node):
-                    if node:
-                        _inorder(node.left)
-                        print(node.key,end="")
-                        _inorder(node.right)
-                _inorder(self.root)
-                print()
+        def in_order(self): # left node right good for sorted order
+            def _inorder(node):
+                if node:
+                    _inorder(node.left)
+                    print(node.key,end="")
+                    _inorder(node.right)
+            _inorder(self.root)
+            print()
 
-            def post_order(self):   # left right root god for deleting trees
-                def _post_order(node):
-                    if node:
-                        _post_order(node.left)
-                        _post_order(node.right)
-                        print(node.key,end="")
-                _post_order(self.root)
-                print()
-            def search(self,target):
-                def _search(node,target):
-                    if node is None:
-                        return None
-                    if node.key == target:
-                        return node
-                    elif target<node.key:
-                        return _search(node.left,target)
-                    else:
-                        return _search(node.right,target)
-                return _search(self.root,target)
+        def post_order(self):   # left right root god for deleting trees
+            def _post_order(node):
+                if node:
+                    _post_order(node.left)
+                    _post_order(node.right)
+                    print(node.key,end="")
+            _post_order(self.root)
+            print()
+        def search(self,target):
+            def _search(node,target):
+                if node is None:
+                    return None
+                if node.key == target:
+                    return node
+                elif target<node.key:
+                    return _search(node.left,target)
+                else:
+                    return _search(node.right,target)
+            return _search(self.root,target)
 
 def main():
     bst = BST()
