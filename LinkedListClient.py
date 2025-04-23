7c8fa3003c5671472acd6f3bdd4d9774316afba5
Daguilar0123 <aguilard107@gmail.com>
2025-04-23 16:49:25 -0400

    Add functionality to insert multiple items into LinkedList and display the list

--- LinkedListClient.py
+++ LinkedListClient.py
@@ -9,12 +9,24 @@ def main():
     ll.insert("Second Element")
     ll.insert("Third Element")
 
+
     print(ll)
     print(ll.first())
     ll.insertAfter("Third Element", "2a")
     print(ll)
     ll.delete("First Element")
     print(ll)
+
+    ll2 = LinkedList()
+    x=int(input("How many items do you want to insert? "))
+    for i in range(x):
+        ll2.insert(input("Enter the first element: "))
+    print(ll2)
+    if x >=2 and x % 2 == 0:
+        print(ll2.delete(str(x-2)))
+    elif x>=1 and x % 2 != 0:
+        print(ll2.delete(str(x-1)))
+    print(ll2)
 
 if __name__=="__main__":
     main()
