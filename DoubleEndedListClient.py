239568ce0b57ae92fae9d333cbe7740ed7d222b2
Daguilar0123 <aguilard107@gmail.com>
2025-04-26 01:29:54 -0400

    Enhance DoubleEndedListClient with additional insert and delete operations

--- DoubleEndedListClient.py
+++ DoubleEndedListClient.py
@@ -18,3 +18,31 @@ print('After inserting', len(dlist) - 1,
       'persons into the linked list after,', after, 'it contains:')
 dlist.traverse()
 print('First:', dlist.first(), 'and Last:', dlist.last())
+next = (404, 'Tim')
+dlist.insertLast(next)
+print('After inserting', next, 'at the end, the double-ended list',
+      'contains:\n', dlist)
+
+dlist.insert(next)
+print('After inserting', next, 'at the front, the double-ended list',
+      'contains:\n', dlist)
+print('Deleting the first item returns', dlist.deleteFirst(),
+      'and leaves the double-ended list containing:\n', dlist,
+      'with first:', dlist.first(), 'and Last:', dlist.last())
+print('Deleting the last item returns',
+      dlist.delete(second(dlist.last()), key=second),
+      'and leaves the double-ended list containing:\n', dlist,
+      'with first:', dlist.first(), 'and Last:', dlist.last())
+
+print('Removing some items from the linked list by key:')
+for person in people[0:5:2]:
+    dlist.delete(person, key=second)
+    print('After deleting', person, 'the list is', dlist)
+    if not dlist.isEmpty():
+        print('The last items is', dlist.last())
+
+print('Removing remaining items from the front of the linked list:')
+while not dlist.isEmpty():
+    print('AFter deleting', dlist.deleteFirst(), 'the list is', dlist)
+    if not dlist.isEmpty():
+        print('The last item is', dlist.last())
