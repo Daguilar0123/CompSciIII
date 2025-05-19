8307d5eee40ec4172963c9d1a2525872d9077f38
Daguilar0123 <aguilard107@gmail.com>
2025-05-18 20:51:18 -0400

    Add initial implementation of Trie data structure with TrieNode class

---
+++ Trie.py
@@ -0,0 +1,19 @@
+#trie_autocorrect.py
+
+class TrieNode:
+    """
+    A single node in the Trie-just like a ListNode in our LinkedList
+    exercises.
+    """
+    def __init__(self):
+        # children is a dict mapping character → TrieNode,
+        # analogous to a 'next' pointer (or multiple 'next's) in ListNode.
+        self.children = {}
+        # is_end flags the end of a valid word, similar to marking the top
+        # of a stick.
+        self.is_end = False
+
+class Trie:
+    """
+    Trie with insert/search and 
+    """
